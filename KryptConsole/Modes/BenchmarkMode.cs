5cfeebeaed99de25a80dcfedfea50cd9568d781c
Vennotius <mcadendorff@gmail.com>
2022-01-10 16:18:21 +0200

    Change GenerateLongString value for Gusto Benchmark

--- KryptConsole/Modes/BenchmarkMode.cs
+++ KryptConsole/Modes/BenchmarkMode.cs
@@ -78,7 +78,7 @@ internal class BenchmarkMode : IMode
 
     private void BenchmarkForGusto()
     {
-        _justALongString = GenerateLongString(2500000);
+        _justALongString = GenerateLongString(6000000);
 
         var backgroundWorker = BackgroundWorkerHelpers.CreateBackgroundWorker();
         backgroundWorker.ProgressChanged += ReportTimeRemaining;
